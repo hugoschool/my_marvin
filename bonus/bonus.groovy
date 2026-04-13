43708ba75e353bd7c6194b52329d91142bd67051
Hugo ARNAL <hugo@hugoarnal.com>
2026-04-13 21:59:21 +0200

    bonus: checking coding style using cs2

---
+++ bonus/bonus.groovy
@@ -0,0 +1,52 @@
+folder('Tools') {
+    displayName('Tools')
+    description('Folder for miscellaneous tools.')
+}
+
+job('Tools/clone-repository') {
+    wrappers {
+        preBuildCleanup {
+            deleteDirectories()
+            cleanupParameter('CLEANUP')
+        }
+    }
+    parameters {
+        stringParam('GIT_REPOSITORY_URL', '', 'Git URL of the repository to clone')
+    }
+    steps {
+        shell('git clone $GIT_REPOSITORY_URL')
+    }
+}
+
+job('Tools/SEED') {
+    displayName('SEED')
+    parameters {
+        stringParam('GITHUB_NAME', '', 'GitHub repository owner/repo_name (e.g.: "EpitechIT31000/chocolatine")')
+        stringParam('DISPLAY_NAME', '', 'Display name for the job')
+    }
+    steps {
+        dsl('''
+            job("$DISPLAY_NAME") {
+                wrappers {
+                    preBuildCleanup {
+                        deleteDirectories()
+                        cleanupParameter('CLEANUP')
+                    }
+                }
+                scm {
+                    github("$GITHUB_NAME")
+                }
+                triggers {
+                    scm('* * * * *')
+                }
+                steps {
+                    shell('cs2 --ci=github')
+                    shell('make fclean')
+                    shell('make')
+                    shell('make tests_run')
+                    shell('make clean')
+                }
+            }
+        ''')
+    }
+}
